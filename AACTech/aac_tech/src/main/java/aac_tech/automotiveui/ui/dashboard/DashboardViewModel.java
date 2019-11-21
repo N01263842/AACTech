80766a1f5c63410344065935ddd0b41e516dc84f
n01263842 <akeemabrakams459@gmail.com>
2019-11-21 10:25:29 -0500

    Continuation. Changed Package name of project
    Continuation.

---
+++ AACTech/aac_tech/src/main/java/aac_tech/automotiveui/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,23 @@
+/*
+ * Team-Name: AAC-Tech
+
+ */
+package aac_tech.automotiveui.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ AACTech/aac_tech/src/main/java/aac_tech/automotiveui/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,23 @@
+/*
+ * Team-Name: AAC-Tech
+
+ */
+package aac_tech.automotiveui.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Completed Emergency Scenarios");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
