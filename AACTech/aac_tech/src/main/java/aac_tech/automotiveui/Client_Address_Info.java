1c63e714e6438ee98d1aacf693c5d52b743e97be
CedricWam <56139872+CedricWam@users.noreply.github.com>
2019-11-25 01:59:59 -0500

    Adding Arrow Up Key

--- AACTech/aac_tech/src/main/java/aac_tech/automotiveui/Client_Address_Info.java
+++ AACTech/aac_tech/src/main/java/aac_tech/automotiveui/Client_Address_Info.java
@@ -14,5 +14,9 @@ public class Client_Address_Info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client__address__info);
+
+        //Setting the title and enabling the up arrow
+        getSupportActionBar().setTitle(R.string.clientInfo_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
