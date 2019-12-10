2e2372444903e8a3dcaa6a8fb6e2dfa430ebeef0
Asma <56137862+asmaa988@users.noreply.github.com>
2019-12-10 12:17:59 -0500

    xml

---
+++ AACTech/aac_tech/src/main/java/aac_tech/automotiveui/UpdateInfo 2.java
@@ -0,0 +1,42 @@
+package aac_tech.automotiveui;
+
+//Will be used to save changed status info to Database
+public class UpdateInfo {
+    private String date;
+    private String activity;
+    private String status;
+
+    public UpdateInfo(){
+
+    }
+
+    public UpdateInfo(String date, String activity, String status) {
+        this.date = date;
+        this.activity = activity;
+        this.status = status;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
