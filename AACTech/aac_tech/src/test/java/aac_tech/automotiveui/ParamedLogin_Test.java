16a61b101db5cfe0509d47f521708a862b6d1774
CedricWam <56139872+CedricWam@users.noreply.github.com>
2019-12-09 21:19:20 -0500

    Added the logig for testing the User name and password

--- AACTech/aac_tech/src/test/java/aac_tech/automotiveui/ParamedLogin_Test.java
+++ AACTech/aac_tech/src/test/java/aac_tech/automotiveui/ParamedLogin_Test.java
@@ -1,7 +1,33 @@
 package aac_tech.automotiveui;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 public class ParamedLogin_Test {
 
     private paramedLogin mparamdLogin;
-    
+
+    @Before
+    public void setUp9(){
+        mparamdLogin = new paramedLogin();
+    }
+
+    @Test
+    public void test_valid_UName(){
+        assertTrue(mparamdLogin.isValidUName("name2345"));
+    }
+
+    @Test
+    public void test_valid_password(){
+        assertTrue(mparamdLogin.isValidPassword("Pass2345"));
+    }
+
+    @Test
+    public void test_NotValid_password(){
+        assertFalse(mparamdLogin.isValidPassword("pass8997"));
+    }
 }
